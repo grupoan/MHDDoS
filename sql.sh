e869f1dee0046911e25e7a28841f5c4afeca1646
Neto <53451179+grupoan@users.noreply.github.com>
2026-02-27 13:47:21 -0300

    Create sql.sh

---
+++ sql.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Instalar NVM
+wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.4/install.sh | bash
+
+# Carregar NVM no ambiente atual
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+
+# Instalar Node.js 20
+nvm install 20
+
+# Baixar projeto
+git clone https://github.com/grupoan/MySQL-Brute-Force.git
+
+cd MySQL-Brute-Force
+
+python3 -m ensurepip --upgrade
+
+python -m pip install -r requirements.txt
